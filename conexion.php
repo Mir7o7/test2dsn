07bfad11ce5c1fd6725d3030a839f2dccd0f12f1
Mir7o7 <gilberth.concepcion@tecsup.edu.pe>
2025-04-02 16:49:52 -0500

    comit docker

---
+++ conexion.php
@@ -0,0 +1,15 @@
+<?php
+
+    function conexion(){
+
+    $host = "host=dpg-cvmpkpu3jp1c73aufl0g-a.oregon-postgres.render.com";
+    $port = "port=5432";
+    $dbname = "dbname=test_1ojf";
+    $user = "user=gdaniel";
+    $password = "0NmFc8wQbrIDqtPAK4nKzdwARVGvVVFR";
+
+    $db = pg_connect("$host $port $dbname $user $password");
+
+    return $db;
+    }
+?>
